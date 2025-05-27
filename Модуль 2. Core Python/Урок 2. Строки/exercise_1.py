707d2f5afba05e42128213c6c8ae030ca11c2426
Ilya Mitrakhovich <jarvizz1303@icloud.com>
2025-05-27 19:52:19 +0200

    sry

--- Модуль 2. Core Python/Урок 2. Строки/exercise_1.py
+++ Модуль 2. Core Python/Урок 2. Строки/exercise_1.py
@@ -2,63 +2,69 @@
 # Результат по каждому заданию необходимо выводить в консоль через print().
 
 # 1. Объедините строки "Hello" и "London" без пробела. Ожидаемый результат: "HelloLondon"
-
+"Hello"+"London"
 
 # 2. Выведите последний символ строки "Programming". Ожидаемый результат: "g"
-
+str="Programming"
+last_char = str[-1]
 
 # 3. Дублируйте строку "Hi" три раза. Ожидаемый результат: "HiHiHi"
-
+'Hi'*3
 
 # 4. Определите длину строки "Artificial Intelligence".
-
-
+str1= "Artificial Intelligence"
+lenght = len(str1)
 # 5. Объедините строки "Good" и "Morning" с запятой между ними. Ожидаемый результат: "Good,Morning"
-
+"Good"+"," +"Morning"
 
 # 6. Создайте срез строки "Natural Language Processing" со значением "nguag".
-
+str2="Natural Language Processing"
+substring=str2[10:15]
 
 # 7. Объедините строки "Data" и "Science" с дефисом между ними. Ожидаемый результат: "Data-Science"
-
+"Data" + "-" + "Science"
 
 # 8. Объедините строки "AI" и "ML" с двоеточием между ними. Ожидаемый результат: "AI:ML"
-
+"AI" + ":" + "ML"
 
 # 9. Дублируйте строку "Test" шесть раз. Ожидаемый результат: "TestTestTestTestTestTest"
-
+'Test'*6
 
 # 10. Выведите первый символ строки "Python". Ожидаемый результат: "P"
-
-
+str3="Python"
+first_char = str[0]
 # 11. Создайте срез строки "Hello, Anna!" от 0 до 5. Ожидаемый результат: "Hello"
-
-
+str4="Hello, Anna!"
+substring=str4[0:5]
 # 12. Определите длину строки "Natural Language Processing".
-
+str5= "Natural Language Processing"
+lenght = len(str5)
 
 # 13. Выведите второй символ строки "Лето". Ожидаемый результат: "е"
-
-
+str6="Лето"
+second_char = str6[1]
 # 14. Выведите предпоследний символ строки "Machine Learning". Ожидаемый результат: "n"
-
+str7="Machine Learning"
+second_char = str7[-2]
 
 # 15. Дублируйте строку "Yes" четыре раза. Ожидаемый результат: "YesYesYesYes"
-
+'Yes'*4
 
 # 16. Создайте срез строки "Deep Learning" со значением "ep Le".
-
-
+str8 = "Deep Learning"
+substring= str8[2:6]
 # 17. Выведите третий символ строки "Computer Vision". Ожидаемый результат: "m"
-
+str9="Machine Learning"
+third_char = str9[2]
 
 # 18. Определите длину строки "Deep Learning". Ожидаемый результат: 13
-
-
+str10="Deep Learning"
+length = len(str10)
 # 19. Объедините строки "Python" и "Rocks" с пробелом между ними. Ожидаемый результат: "Python Rocks"
-
+"Python" + " " + "Rocks"
 
 # 20. Создайте срез строки "Data Science" со значением "cien".
-
+str11="Data Science"
+substring = str11[6:9]
 
 
