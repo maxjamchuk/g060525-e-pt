ad2506b3389267ab0b0148b5da1f6e3852e1019b
Zoriana <zirka9696@gmail.com>
2025-05-28 17:53:41 +0200

    hw2

--- Модуль 2. Core Python/Урок 2. Строки/exercise_2.py
+++ Модуль 2. Core Python/Урок 2. Строки/exercise_2.py
@@ -1,5 +1,6 @@
 # Тема: Основные методы строк
 # Результат по каждому заданию необходимо выводить в консоль через print().
+from os.path import split
 
 # 1. Преобразуйте строку "hOw aRe yOu?" в верхний регистр.
 # Ожидаемый результат: "HOW ARE YOU?"
@@ -19,38 +20,57 @@ print(str3.lower())
 
 # 4. Удалите начальные пробелы из строки "   Discover new worlds   ".
 # Ожидаемый результат: "Discover new worlds   "
+str4 = "   Discover new worlds   "
+print(str4.lstrip())
 
 
 # 5. Замените "rainy" на "sunny" в строке "It was a rainy day.".
 # Ожидаемый результат: "It was a sunny day."
-
+str5 = "It was a rainy day."
+print(str5.replace("rainy", "sunny"))
 
 # 6. Найдите индекс подстроки "innovation" в строке "Innovation drives progress.".
+str6 = "Innovation drives progress."
+word_6 = "innovation"
+index_6 = str6.lower().find(word_6)
+print(f"индекс подстроки:{index_6}")
 
 
 # 7. Удалите конечные пробелы из строки "   Explore the universe   ".
 # Ожидаемый результат: "   Explore the universe"
+str7 = "   Explore the universe   "
+print(str4.rstrip())
 
 
 # 8. Найдите индекс подстроки "galaxy" в строке "The Milky Way galaxy is vast.".
+str8 = "The Milky Way galaxy is vast."
+word_8 = "galaxy"
+index_8 =str8.lower().find(word_8)
+print(f"индекс подстроки:{index_8}")
 
 
 # 9. Разделите строку "Apple;Banana;Cherry;Date" по точке с запятой.
 # Ожидаемый результат: ["Apple", "Banana", "Cherry", "Date"]
-
+str9 ="Apple;Banana;Cherry;Date"
+print(str9.split(";"))
 
 # 10. Замените "robots" на "humans" в строке "In the future, robots will rule the world.".
 # Ожидаемый результат: "In the future, humans will rule the world."
-
+str10 = "In the future, robots will rule the world."
+print(str10.replace("robots","humans"))
 
 # 11. Преобразуйте строку "artificial intelligence" в заглавный регистр.
 # Ожидаемый результат: "Artificial Intelligence"
-
+str_11 = "Artificial Intelligence"
+print(str_11.upper())
 
 # 12. Разделите строку "Python is a versatile language" по пробелам.
 # Ожидаемый результат: ["Python", "is", "a", "versatile", "language"]
+str12 = "Python is a versatile language"
+print(str12.split(" "))
 
 
 # 13. Удалите начальные и конечные пробелы из строки "   Learn Python   ".
 # Ожидаемый результат: "Learn Python"
-
+str13 = "   Learn Python   "
+print(str13.strip())
--- Модуль 2. Core Python/Урок 2. Строки/exercise_3.py
+++ Модуль 2. Core Python/Урок 2. Строки/exercise_3.py
@@ -5,72 +5,105 @@
 # Ожидаемый результат:
 # Hello
 # World
+print("Hello\nWorld")
 
 
 # 2. Вставьте символ обратного слэша в строку "This is a backslash: ".
 # Ожидаемый результат: "This is a backslash: \"
+str2 = "This is a backslash: \\"
+print(str2)
 
 
 # 3. Экранируйте кавычки в строке "He said, "Hello!"".
 # Ожидаемый результат: He said, "Hello!"
+str3 = "He said, \"Hello!\""
+print(str3)
 
 
 # 4. Экранируйте одинарные кавычки в строке "It's a sunny day".
 # Ожидаемый результат: It's a sunny day
+print("It\'s a sunny day")
 
 
 # 5. Вставьте символ новой строки в строку "Python Programming".
 # Ожидаемый результат:
 # Python
 # Programming
-
+print("Python\nProgramming")
 
 # 6. Экранируйте кавычки в строке "She said, 'Hi!'".
 # Ожидаемый результат: She said, 'Hi!'
-
+print("She said, \'Hi!\'")
 
 # 7. Экранируйте обратный слэш в строке "Path to file: C:\\".
 # Ожидаемый результат: Path to file: C:\\
-
+print("Path to file: C:\\\\")
 
 # 8. Используйте метод `format()` для строки "This is a ... course for ... learners." с переменными course="Python"
 # и level="beginner". Ожидаемый результат: "This is a Python course for beginner learners."
+course="Python"
+level="beginner"
+print("This is a {} course for {} learners.".format(course,level))
+
 
 
 # 9. Используйте F-строку для строки "This is a ... course for ... learners." с переменными
 # course="Python" и level="beginner". Ожидаемый результат: "This is a Python course for beginner learners."
-
+course="Python"
+level="beginner"
+print(f"This is a {course} course for {level} learners.")
 
 # 10. Используйте метод `format()` для строки "Welcome to the ... workshop." с переменной topic="Machine Learning".
 # Ожидаемый результат: "Welcome to the Machine Learning workshop."
-
+workshop="Machine Learning"
+print("Welcome to the {} workshop.".format(workshop))
 
 # 11. Используйте F-строку для строки "Welcome to the ... workshop." с переменной topic="Machine Learning".
 # Ожидаемый результат: "Welcome to the Machine Learning workshop."
+topic="Machine Learning"
+print(f"Welcome to the{topic}workshop.")
 
 
 # 12. Придумайте название переменной и поместите в нее строку "machine learning",
 # затем преобразуйте первые буквы слов в заглавный регистр, чтобы получилось "Machine Learning".
 # Затем создайте переменную со строкой "Course: ". Используйте метод `format()`, чтобы показать в консоле
 # "Course: Machine Learning"
-
+str12 = "machine learning"
+str12_1 = str12.title()
+course1 = "Course: "
+print(" {} {}".format(course1, str12_1))
 
 # 13. Объедините строки "Data" и "Science" с пробелом между ними, дублируйте результат три раза, и используйте F-строку
 # для строки "Field: ...". Ожидаемый результат: "Field: Data ScienceData ScienceData Science"
+str13 = "Data" +" "+ "Science"
+str13_1 = str13*3
+print(f"Field:{str13_1}")
 
 
 # 14. Выведите третий символ строки "Information", затем используйте метод `format()` для строки "Third character: ...".
 # Ожидаемый результат: "Third character: f"
-
+str14 = "Information"
+str14_1 = str14[2]
+print("Third character:{}".format(str14_1))
 
 # 15. Определите длину строки "Neural Networks", умножьте её на 2, и используйте F-строку для строки "Length: ".
 # Ожидаемый результат: "Length: 28"
+str15 = "Neural Networks"*2
+str15_1 = len(str15)
+print(f"Length: {str15_1}")
+
 
 
 # 16. Преобразуйте строку "Deep Learning" в заглавный регистр, найдите индекс подстроки "LEARNING", и выведите символ
 # на этом индексе. Ожидаемый результат: "L"
-
+str16 = "Deep Learning".upper()
+index16 =str16.find("LEARNING")
+result = str16[index16]
+print(result)
 
 # 20. Определите длину строки "Starta", затем преобразуйте её в строку и добавьте к строке " has length of ",
 # используя метод `format()`. Ожидаемый результат: "Starta has length of 6"
+str20 = "Starta"
+str20_1 = len(str20)
+print("{} has length of {}".format(str20,str20_1))
 
