6051d0a42eefc6f87105ad4d755404ae58017040
Alexandr Meshchriakov <avm@sh-inc.ru>
2025-05-30 19:04:15 +0200

    feat: add examples and exercises for list manipulation and conditional statements

--- Модуль 2. Core Python/Урок 3. Условия/live-coding.py
+++ Модуль 2. Core Python/Урок 3. Условия/live-coding.py
@@ -1,4 +1,18 @@
+
 # Продемонстрируйте работу булевых переменных и операторов сравнения.
+print("Булевы переменные и операторы сравнения:")
+# Булевы переменные могут принимать значения True или False.
+is_raining = True
+is_sunny = False
+print(f"Is it raining? {is_raining}")
+print(f"Is it sunny? {is_sunny}")
+print("Операторы сравнения:")
+print(5 > 3)  # True
+print(5 == 3)  # False
+print(5 != 3)  # False
+print(5 <= 3)  # False
+print(5 >= 3)  # False
+
 # Продемонстрируйте работу условных операторов if и if-else.
 
 
@@ -7,6 +21,15 @@
 # Продемонстрируйте и объясните работу:
 # - Вложенных условий
 # - Конструкции if-elif-else
+a = 10
+
+if a == 1:
+    print("a равно 1")
+elif a == 2:
+    print("a равно 2")
+elif a == 3:
+    print("a равно 3")
+
 
 
 # Логические операторы: and, or, not
