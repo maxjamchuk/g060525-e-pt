7d3d3bc827fa4a8d3e8b6c155fffed8c74595ee5
Ilya Mitrakhovich <jarvizz1303@icloud.com>
2025-06-17 18:58:13 +0200

    sry

--- Модуль 2. Core Python/Урок 10. Рекурсия/exercise_2.py
+++ Модуль 2. Core Python/Урок 10. Рекурсия/exercise_2.py
@@ -3,15 +3,27 @@
 # 1. Напишите функцию `sum_of_digits(n)`, которая возвращает сумму цифр числа `n` с помощью рекурсии.
 # Пример использования:
 # print(sum_of_digits(12345))  # Вывод: 15
-
+def sum_of_digits(n):
+    if n < 10:
+        return n
+    return n % 10 + sum_of_digits(n // 10)
+print(sum_of_digits(98))
 
 # 2. Напишите функцию `reverse_string(s)`, которая возвращает строку `s` в обратном порядке с помощью рекурсии.
 # Пример использования:
 # print(reverse_string("hello"))  # Вывод: "olleh"
-
-
+def reverse_string(s):
+    if len(s) <= 1:
+        return s
+    return reverse_string(s[1:])+ s[0]
+print(reverse_string("hello"))
 
 # 3. Напишите функцию `list_length(lst)`, которая возвращает длину списка `lst` с помощью рекурсии.
 # Пример использования:
 # print(list_length([1, 2, 3, 4, 5]))  # Вывод: 5
+def list_length(lst):
+    if lst == []:
+        return 0
+    return 1+ list_length(lst[1:])
 
+print(list_length([1, 2, 3, 4, 5]))
--- Модуль 2. Core Python/Урок 10. Рекурсия/exercise_3.py
+++ Модуль 2. Core Python/Урок 10. Рекурсия/exercise_3.py
@@ -5,23 +5,53 @@
 # и возвращает их произведение.
 # Пример использования:
 # print(multiply_all(1, 2, 3, 4))  # Вывод: 24
-
+def multiply_all(*args):
+    if not args:
+        return 1
+    return args[0] * multiply_all(*args[1:])
+print(multiply_all(1, 2, 3, 4))
 
 
 # 2. Напишите функцию `merge_dicts`, которая принимает произвольное
 # количество словарей с помощью `**kwargs`
 # и возвращает один объединённый словарь.
-# Пример использования:
+# # Пример использования:
 # dict1 = {"a": 1, "b": 2}
 # dict2 = {"c": 3, "d": 4}
 # print(merge_dicts(**dict1, **dict2))  # Вывод: {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+def merge_dicts(**kwargs):
+    if not kwargs:
+        return {}
+    keys = list(kwargs.keys())
+    if len(keys) == 1:
+        return {keys[0]: kwargs[keys[0]]}
 
-
+    first_key = keys[0]
+    first_value = kwargs.pop(first_key)
+    rest_merged = merge_dicts(**kwargs)
+    rest_merged[first_key] = first_value
+    return rest_merged
+dict1 = {"a": 1, "b": 2}
+dict2 = {"c": 3, "d": 4}
+print(merge_dicts(**dict1, **dict2))
 # 3. Напишите функцию `make_flatten`, которая создаёт
 # функцию `flatten`, превращающую вложенный список в одноуровневый.
 # Пример использования:
 # flatten = make_flatten()
 # print(flatten([1, [2, [3, 4], 5], 6]))  # Вывод: [1, 2, 3, 4, 5, 6]
+def make_flatten():
+    def flatten(lst):
+        result=[]
+        for item in lst:
+            if isinstance(item, list):
+                result.extend(flatten(item))
+            else:
+                result.append(item)
+        return result
+    return flatten
+flatten = make_flatten()
+print(flatten([1, [2, [3, 4], 5], 6]))
+
 
 
 # 4. Напишите функцию `show_info`, которая принимает
@@ -34,3 +64,9 @@
 # Вывод:
 # Args: (1, 2, 3)
 # Kwargs: {'name': 'Alice', 'age': 30}
+def show_info(*args, **kwargs):
+    print("Args:", args)
+    print("Kwargs:", kwargs)
+args = (1, 2, 3)
+kwargs = {"name": "Alice", "age": 30}
+show_info(*args, **kwargs)
